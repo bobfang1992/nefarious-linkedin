79ad42dc340f5092133d6355c9225374fbc97f83
Corey Prophitt <prophittcorey@users.noreply.github.com>
2019-06-17 13:23:02 -0700

    Added ability to save the JSON file

--- extension/js/browser-action.js
+++ extension/js/browser-action.js
@@ -1,7 +1,51 @@
+//
+// Code snippet taken and modified from Stackoverflow:
+//  https://stackoverflow.com/questions/11849562/how-to-save-the-output-of-a-console-logobject-to-a-file
+//
+function saveJson(data, filename) {
+  if (!data) {
+    console.error("Console.save: No data");
+    return;
+  }
+
+  if (!filename) {
+    filename = "console.json";
+  }
+
+  if (typeof data === "object") {
+    data = JSON.stringify(data, undefined, 2)
+  }
+
+  var blob = new Blob([data], { type: "text/json" }),
+    e    = document.createEvent("MouseEvents"),
+    a    = document.createElement("a")
+
+  a.download = filename
+  a.href = window.URL.createObjectURL(blob)
+  a.dataset.downloadurl =  ["text/json", a.download, a.href].join(":")
+  e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+  a.dispatchEvent(e)
+}
+
 function removeLoader() {
   var loadingContainer = document.querySelector('.loading-container');
 
   document.body.removeChild(loadingContainer);
+}
+
+function showErrorView() {
+  document.querySelector(".error").classList.remove("hidden");
+}
+
+function showDataView(data) {
+  document.querySelector(".actions").classList.remove("hidden");
+
+  document.querySelector('.download').addEventListener('click', function () {
+    saveJson(data, "linkedin-" + (new Date().getTime()) + ".json");
+    return false;
+  });
+
+  document.querySelector(".data").classList.remove("hidden");
 }
 
 window.onload = function () {
@@ -17,13 +61,13 @@ window.onload = function () {
       removeLoader();
 
       if (!r || ![0]) {
-        // TODO: Show not found for some reason.
+        showErrorView();
         return;
       }
 
       var localFile = JSON.parse(atob(r[0]));
 
-      console.log(localFile);
+      showDataView(localFile);
     });
   });
 };
