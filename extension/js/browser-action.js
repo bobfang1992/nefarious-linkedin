79ed23d90403fe6d08699dd5aa356cf4cf83baa7
Corey Prophitt <prophittcorey@users.noreply.github.com>
2019-06-17 14:30:44 -0700

    Added save CSV functionality

--- extension/js/browser-action.js
+++ extension/js/browser-action.js
@@ -17,14 +17,80 @@ function saveJson(data, filename) {
   }
 
   var blob = new Blob([data], { type: "text/json" }),
-    e    = document.createEvent("MouseEvents"),
-    a    = document.createElement("a")
+      e    = document.createEvent("MouseEvents"),
+      a    = document.createElement("a")
 
   a.download = filename
   a.href = window.URL.createObjectURL(blob)
   a.dataset.downloadurl =  ["text/json", a.download, a.href].join(":")
   e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
   a.dispatchEvent(e)
+}
+
+//
+// Code snippet taken and modified from Stackoverflow:
+//  https://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
+//
+function exportToCsv(filename, rows) {
+  var processRow = function (row) {
+    var finalVal = '';
+    for (var j = 0; j < row.length; j++) {
+      var innerValue = row[j] === null ? '' : row[j].toString();
+      if (row[j] instanceof Date) {
+        innerValue = row[j].toLocaleString();
+      };
+      var result = innerValue.replace(/"/g, '""');
+        if (result.search(/("|,|\n)/g) >= 0)
+          result = '"' + result + '"';
+          if (j > 0)
+          finalVal += ',';
+          finalVal += result;
+        }
+    return finalVal + '\n';
+  };
+
+  var csvFile = '';
+  for (var i = 0; i < rows.length; i++) {
+    csvFile += processRow(rows[i]);
+  }
+
+  var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+  if (navigator.msSaveBlob) { // IE 10+
+    navigator.msSaveBlob(blob, filename);
+  } else {
+    var link = document.createElement("a");
+    if (link.download !== undefined) { // feature detection
+      // Browsers that support HTML5 download attribute
+      var url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", filename);
+      link.style.visibility = 'hidden';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  }
+}
+
+function exportCsv(data) {
+  var filename = "linkedin-" + (new Date().getTime()) + ".csv";
+  var rows = [ ["extension_name", "webstore_link"] ];
+
+  data.Metadata.ext.sort(compare).forEach(function (e) {
+    var link = "";
+
+    if (e.path && e.path[0]) {
+      var extensionId = e.path[0].split("/")[0];
+      link = "https://chrome.google.com/webstore/detail/" + extensionId;
+    }
+
+    rows.push([
+      decode(e.name),
+      link,
+    ]);
+  });
+
+  exportToCsv(filename, rows);
 }
 
 function decode(str) {
@@ -113,8 +179,13 @@ function showErrorView() {
 }
 
 function showDataView(data) {
-  document.querySelector('.download').addEventListener('click', function () {
+  document.querySelector(".download").addEventListener('click', function () {
     saveJson(data, "linkedin-" + (new Date().getTime()) + ".json");
+    return false;
+  });
+
+  document.querySelector(".export").addEventListener("click", function () {
+    exportCsv(data);
     return false;
   });
 
