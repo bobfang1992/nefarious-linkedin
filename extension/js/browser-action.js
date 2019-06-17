240d83841b5675c42a1bc8374ef23285edd4dde2
Corey Prophitt <prophittcorey@users.noreply.github.com>
2019-06-17 12:18:54 -0700

    Show a loader while getting local storage data

--- extension/js/browser-action.js
+++ extension/js/browser-action.js
@@ -6,8 +6,16 @@ window.onload = function () {
     chrome.tabs.executeScript(tab.id, {
       code: 'localStorage.getItem("C_C_M");'
     }, function (r) {
-      console.log(r);
       chrome.tabs.remove(tab.id);
+
+      if (!r || ![0]) {
+        // TODO: Show not found for some reason.
+        return;
+      }
+
+      var localFile = JSON.parse(atob(r[0]));
+
+      console.log(localFile);
     });
   });
 };
